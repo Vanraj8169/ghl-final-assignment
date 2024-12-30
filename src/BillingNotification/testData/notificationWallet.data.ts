4d49cae411e409fb55951758603df2bfe1703912
Vanraj Pardeshi <87256781+Vanraj8169@users.noreply.github.com>
2024-12-30 14:34:28 +0530

    Update notificationWallet.data.ts

--- src/BillingNotification/testData/notificationWallet.data.ts
+++ src/BillingNotification/testData/notificationWallet.data.ts
@@ -4,3 +4,10 @@ export const homePageData = {
 }
 
 export const notificationWalletUsageText = "Set up usage notifications to effectively manage your agency wallet spending";
+export const emailNotificationText = "Email Notification will be sent for Above sub-accounts Wallet transactions";
+
+export const agencyOwnerEmail = "viveks+test@gohighlevel.com";
+export const notifyEmailAddressText = "You can add upto 5 email address to get notified about wallet usage.";
+
+
+export const testEmails:string[] = [];
