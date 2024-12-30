e4629c6a9f90aa5edce069f6c23c1b62716ced17
Vanraj Pardeshi <87256781+Vanraj8169@users.noreply.github.com>
2024-12-30 14:33:07 +0530

    Update login.page.ts

--- src/BillingNotification/pageFactory/pageRepository/login.page.ts
+++ src/BillingNotification/pageFactory/pageRepository/login.page.ts
@@ -8,29 +8,13 @@ export class Login {
     this.page = page;
   }
 
-  // Below function is used to interact with element
-  private async interactWithElement(
-    action: "fill" | "click",
-    locator: string,
-    value?: string
-  ) {
-    const element = this.page.locator(locator);
-
-    switch (action) {
-      case "fill":
-        if (value) await element.fill(value);
-        break;
-      case "click":
-        await element.click();
-        break;
-    }
-  }
   async signInAndNavigateToDashboard(username: string, password: string) {
-    await this.interactWithElement("fill",loginObject.emailInput,username);
-    await this.interactWithElement("fill",loginObject.passwordInput,password);
+    await this.page.fill(loginObject.emailInput, username);
+    await this.page.fill(loginObject.passwordInput, password);
 
-    await this.interactWithElement("click", loginObject.signInBtn);
-    await this.interactWithElement("click",loginObject.testLAccount);
+    await this.page.locator(loginObject.signInBtn).click();
+    await this.page.locator(loginObject.testLAccount).click();
+
     await this.page.waitForURL(homePageData.agencyDashboardUrl);
   }
 }
